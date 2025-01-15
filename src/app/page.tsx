adec689ee29907d069abaf48fd6a971ef99f2bfa
briannabouassali <113944392+briannabouassali@users.noreply.github.com>
2025-01-15 18:07:23 -0500

    Implemented login page UI (#2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function page() {
-	<div>Hello</div>
-}
+export default function Home() {
+	return (
+	  <div style={{ padding: '2rem', fontSize: '1.5rem', color: 'blue' }}>
+		Home Page Loaded! Go to <a href="/login">Login Page</a>.
+	  </div>
+	);
+  }
+  
