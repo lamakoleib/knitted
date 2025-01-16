df802fbeb4f611d06ffdb34922a31eabf9522dbc
Lama Koleib <lkole095@uottawa.ca>
2025-01-15 23:08:54 -0500

    Created post card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
 import "./globals.css";
 export default function Home() {
-	return (
-		<div style={{ padding: "2rem", fontSize: "1.5rem", color: "blue" }}>
-			Home Page Loaded! Go to <a href="/login">Login Page</a>.
-		</div>
-	);
+	return <>Hello</>;
 }
--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -38,7 +38,8 @@ export async function updateSession(request: NextRequest) {
 	if (
 		!user &&
 		!request.nextUrl.pathname.startsWith("/login") &&
-		!request.nextUrl.pathname.startsWith("/signup")
+		!request.nextUrl.pathname.startsWith("/signup") &&
+		false
 	) {
 		// no user, potentially respond by redirecting the user to the login page
 		const url = request.nextUrl.clone();
