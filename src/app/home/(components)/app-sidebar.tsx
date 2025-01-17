b6511ff6078fc2cc52d615c692e6b8628bd55b9f
lamakoleib <114184666+lamakoleib@users.noreply.github.com>
2025-01-17 14:45:08 -0500

    end-to-end auth (#6)
    ui: auth: Implemented UI for login, logout and signup with auth

---
+++ src/app/home/(components)/app-sidebar.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import * as React from "react";
+import {
+	AudioWaveform,
+	Frame,
+	Map,
+	Search,
+	Volleyball,
+	Rss,
+} from "lucide-react";
+
+import { NavUser } from "./nav-user";
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarHeader,
+	SidebarRail,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarGroup,
+	SidebarGroupLabel,
+} from "@/components/ui/sidebar";
+
+const data = {
+	user: {
+		name: "shadcn",
+		email: "m@example.com",
+		avatar: "https://github.com/shadcn.png",
+	},
+	options: [
+		{
+			name: "Feed",
+			url: "#",
+			icon: Rss,
+		},
+		{
+			name: "Explore",
+			url: "#",
+			icon: Map,
+		},
+		{
+			name: "Search",
+			url: "#",
+			icon: Search,
+		},
+		{
+			name: "Yarn",
+			url: "#",
+			icon: AudioWaveform,
+		},
+		{
+			name: "Patterns",
+			url: "#",
+			icon: Frame,
+		},
+	],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+	return (
+		<Sidebar collapsible="icon" {...props}>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton size="lg" asChild>
+							<a href="#">
+								<div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-rose-300 text-sidebar-primary-foreground">
+									<Volleyball className="size-4" />
+								</div>
+								<div className="flex flex-col gap-0.5 leading-none">
+									<span className="font-semibold">Knitted</span>
+								</div>
+							</a>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent>
+				<SidebarGroup className="group-data-[collapsible=icon]:hidden">
+					<SidebarGroupLabel>Application</SidebarGroupLabel>
+					<SidebarMenu>
+						{data.options.map((item) => (
+							<SidebarMenuItem key={item.name}>
+								<SidebarMenuButton size="lg" asChild>
+									<a href={item.url}>
+										<item.icon />
+										<span>{item.name}</span>
+									</a>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+						))}
+					</SidebarMenu>
+				</SidebarGroup>
+			</SidebarContent>
+			<SidebarFooter>
+				<NavUser user={data.user} />
+			</SidebarFooter>
+			<SidebarRail />
+		</Sidebar>
+	);
+}
--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -41,8 +41,7 @@ export async function updateSession(request: NextRequest) {
 	if (
 		!user &&
 		!request.nextUrl.pathname.startsWith("/login") &&
-		!request.nextUrl.pathname.startsWith("/signup") &&
-		false
+		!request.nextUrl.pathname.startsWith("/signup")
 	) {
 		// no user, potentially respond by redirecting the user to the login page
 		const url = request.nextUrl.clone();
