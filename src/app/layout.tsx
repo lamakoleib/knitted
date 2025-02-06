78da6fdb87ff14160da24a7845ffaaf899b4252d
Lama Koleib <lkole095@uottawa.ca>
2025-02-06 17:51:32 -0500

    ui: added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
-import "./globals.css";
+import "./globals.css"
 export default function RootLayout({
 	children,
 }: Readonly<{
-	children: React.ReactNode;
+	children: React.ReactNode
 }>) {
 	return (
 		<html lang="en">
+			<meta name="apple-mobile-web-app-title" content="knitted" />
+			<link rel="shortcut icon" href="/favicon.ico" />
+			<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+
 			<body>{children}</body>
 		</html>
-	);
+	)
 }
