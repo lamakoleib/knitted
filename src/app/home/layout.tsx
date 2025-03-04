b9137896ff1cfdf2dd084ef847556cdf990a25c2
Lama Koleib <lkole095@uottawa.ca>
2025-03-03 20:15:56 -0500

    fixed merge conflicts

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,48 +1,42 @@
 import {
-	SidebarProvider,
-	SidebarTrigger,
-	SidebarInset,
-} from "@/components/ui/sidebar"
-import { AppSidebar } from "./components/app-sidebar"
+  SidebarProvider,
+  SidebarTrigger,
+  SidebarInset,
 } from "@/components/ui/sidebar"
 import { AppSidebar } from "./components/app-sidebar"
 import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbList,
-	BreadcrumbLink,
-	BreadcrumbSeparator,
-	BreadcrumbPage,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
 } from "@/components/ui/breadcrumb"
 import { Separator } from "@/components/ui/separator"
 export default async function Layout({
-	children,
+  children,
 }: {
-	children: React.ReactNode
-	children: React.ReactNode
+  children: React.ReactNode
 }) {
-	return (
-		<SidebarProvider>
-			<AppSidebar />
-			<SidebarInset>
-				<header className="flex h-16 bg-muted shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-					<div className="flex items-center gap-2 px-4">
-						<SidebarTrigger className="-ml-1" />
-						<Separator orientation="vertical" className="mr-2 h-4" />
-						<Breadcrumb>
-							<BreadcrumbList>
-								<BreadcrumbItem className="hidden md:block">
-									<BreadcrumbLink href="/home">Home</BreadcrumbLink>
-								</BreadcrumbItem>
-							</BreadcrumbList>
-						</Breadcrumb>
-					</div>
-				</header>
-				<div className="bg-muted w-full h-full">{children}</div>
-			</SidebarInset>
-		</SidebarProvider>
-	)
-	)
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 bg-muted shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="/home">Home</BreadcrumbLink>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="bg-muted w-full h-full">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
